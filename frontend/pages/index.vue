9ef4aed1cfbc002797e849a1fd5c713e3d2d7733
Kaiwen Song <31936108+Kaidriver@users.noreply.github.com>
2024-05-20 23:08:04 -0400

    make markers hoverable

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -9,18 +9,55 @@
             <ol-source-osm />
         </ol-tile-layer>
     </ol-map>   
+    <div id="popup" class="ol-popup">
+      <a href="#" id="popup-closer" class="ol-popup-closer"></a>
+      <div id="popup-content"></div>
+    </div>
 </template>
 
 <style scoped>
     #map {
         width: 50%;
     }
+    .ol-popup {
+        position: absolute;
+        background-color: white;
+        box-shadow: 0 1px 4px rgba(0,0,0,0.2);
+        padding: 15px;
+        border-radius: 10px;
+        border: 1px solid #cccccc;
+        bottom: 12px;
+        left: -50px;
+        min-width: 280px;
+      }
+      .ol-popup:after, .ol-popup:before {
+        top: 100%;
+        border: solid transparent;
+        content: " ";
+        height: 0;
+        width: 0;
+        position: absolute;
+        pointer-events: none;
+      }
+      .ol-popup:after {
+        border-top-color: white;
+        border-width: 10px;
+        left: 48px;
+        margin-left: -10px;
+      }
+      .ol-popup:before {
+        border-top-color: #cccccc;
+        border-width: 11px;
+        left: 48px;
+        margin-left: -11px;
+      }
 </style>
 <script setup lang="ts">
     import 'vue3-openlayers/dist/vue3-openlayers.css';
     
     import Point from 'ol/geom/Point.js'
     import Circle from 'ol/geom/Circle.js';
+    import Overlay from 'ol/Overlay.js';
     import {Icon, Style} from 'ol/style'
     import Feature from 'ol/Feature.js'
     import * as olProj from 'ol/proj';
@@ -55,7 +92,10 @@
         let properties = JSON.parse(JSON.stringify(data.value))
         let featuresList = []
         for (let i = 0; i < properties.length; i++) {
-            featuresList.push({'type': 'Feature', 'geometry': JSON.parse(properties[i].geojson)})
+            featuresList.push({'type': 'Feature', 'geometry': JSON.parse(properties[i].geojson), 'properties': {
+                'address': properties[i].street + " " + (properties[i].street2 == null ? "" : properties[i].street2 + " ") + ", " + properties[i].city + " " + properties[i].st + ", " + properties[i].postal,
+                'post_date': properties[i].post_date
+            }})
         }
 
         let geojsonObject = {
@@ -73,6 +113,9 @@
     }
 
     onMounted(() => {
+        const container = document.getElementById('popup')!;
+        const content = document.getElementById('popup-content')!;
+
         const mapsScript = document.createElement("script");
         mapsScript.setAttribute(
             "src",
@@ -91,6 +134,17 @@
         });
 
         map.addLayer(vectorLayer1 as any); 
+
+        const overlay = new Overlay({
+            element: container,
+            autoPan: {
+                animation: {
+                duration: 250,
+                },
+            },
+        });
+        map.addOverlay(overlay as any)
+
         map.on("moveend", async function(e){
             let newFeatures = await queryPropertiesFromView(map)
             vectorSource1.addFeatures(newFeatures)
@@ -147,9 +201,32 @@
                     geometry: markerGeometry
                 });
 
-               
+                markerFeature.addEventListener('mouseover', () => {
+                    alert("hover!")
+                })
+
                 let retrieveFeatures = await queryPropertiesFromView(map)
                 vectorSource1.addFeatures([poly, markerFeature, ...retrieveFeatures])
+                
+
+                map.on('pointermove', function (e: any) {
+                    let onFeature = false
+                    map.forEachFeatureAtPixel(e.pixel, function (f) {
+                        //TODO: in case of duplicate hover (more than one property), add on to overlay instead of setting it
+                        //also remove if statement
+                        if (f.get("address") != undefined) {
+                            content.innerHTML=`<p>${f.get("address")}`
+                            overlay.setPosition(e.coordinate)
+                            console.log(e.coordinate)
+
+                            onFeature = true
+                        }
+                    });
+
+                    if (!onFeature) {
+                        overlay.setPosition(undefined)
+                    }
+                });
             })
             
         }
